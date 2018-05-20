b09fdaa0c6436c577c34e8e43159e4bd88c85d15
Garrett Cullen <Garth619@users.noreply.github.com>
2018-05-20 12:38:16 -0700

    push

--- wp-content/themes/starter-theme/functions.php
+++ wp-content/themes/starter-theme/functions.php
@@ -24,89 +24,99 @@ add_theme_support('post-thumbnails');
 
 
 
-/* Lawyer Firm CPT 
+/* Search Tweaks
 -------------------------------------------------------------- */
 
 
- 	
-      
-/*
-   	 
-	function lawfirm() {    
-    	$args = array(    
-        	'label' => __('Lawfirms'),    
-        	'singular_label' => __('Lawfirm'),    
-        	'public' => true,    
-        	'show_ui' => true,
-        	// 'taxonomies' => array('lawfirmcat'),
-        	'has_archive' => true,	 
-        	'capability_type' => 'post',    
-        	'hierarchical' => true,    
-        	'rewrite' => true,
-        	'show_in_rest' => true,    
-        	'supports' => array( 'title', 'editor','page-attributes', 'author', 'thumbnail', 'excerpt', 'comments' )    
-       	);    
-   	 
-    	register_post_type( 'lawfirm' , $args );    
-	}  
-	
-	
-	add_action('init', 'lawfirm');
-	
-	  
-	register_taxonomy(
-		"lawfirmcat", 
-		array("lawfirm"),
-		array(
-			"hierarchical" => true,
-			'show_in_rest' => true,
-			"label" => "Lawfirm Cats",
-			"singular_label" => "Lawfirm Cat",
-			'rewrite' => array( 'slug' => 'lawyer' )
-		)); 
-
-
-
-
-	add_action('init', 'createlawfirmtags');
-	
-	
-	function createlawfirmtags() {
- 
-// Labels part for the GUI
- 
-  $labels = array(
-    'name' => _x( 'Practice Area Tags', 'taxonomy general name' ),
-    'singular_name' => _x( 'Practice Area Tag', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Practice Area Tags' ),
-    'popular_items' => __( 'Popular Practice Area Tags' ),
-    'all_items' => __( 'All Practice Area Tags' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit Practice Area Tag' ), 
-    'update_item' => __( 'Update Practice Area Tag' ),
-    'add_new_item' => __( 'Add New Practice Area Tag' ),
-    'new_item_name' => __( 'New Practice Area Tag Name' ),
-    'separate_items_with_commas' => __( 'Separate Practice Area Tags with commas' ),
-    'add_or_remove_items' => __( 'Add or remove topics' ),
-    'choose_from_most_used' => __( 'Choose from the most used Practice Area Tags' ),
-    'menu_name' => __( 'Practice Area Tags' ),
-  ); 
- 
-// Now register the non-hierarchical taxonomy like tag
- 
-  register_taxonomy('lawfirmtags','lawfirm',array(
-    'hierarchical' => false,
-    'labels' => $labels,
-    'show_ui' => true,
-    'show_in_rest' => true,
-    'show_admin_column' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'practicareas' ),
-  ));
+/**
+ * [list_searcheable_acf list all the custom fields we want to include in our search query]
+ * @return [array] [list of custom fields]
+ */
+function list_searcheable_acf(){
+  $list_searcheable_acf = array("attorney_name");
+  return $list_searcheable_acf;
 }
-*/
+
+
+/**
+ * [advanced_custom_search search that encompasses ACF/advanced custom fields and taxonomies and split expression before request]
+ * @param  [query-part/string]      $where    [the initial "where" part of the search query]
+ * @param  [object]                 $wp_query []
+ * @return [query-part/string]      $where    [the "where" part of the search query as we customized]
+ * see https://vzurczak.wordpress.com/2013/06/15/extend-the-default-wordpress-search/
+ * credits to Vincent Zurczak for the base query structure/spliting tags section
+ */
+function advanced_custom_search( $where, &$wp_query ) {
+
+    global $wpdb;
+ 
+    if ( empty( $where ))
+        return $where;
+ 
+    // get search expression
+    $terms = $wp_query->query_vars[ 's' ];
+    
+    // explode search expression to get search terms
+    $exploded = explode( ' ', $terms );
+    if( $exploded === FALSE || count( $exploded ) == 0 )
+        $exploded = array( 0 => $terms );
+         
+    // reset search in order to rebuilt it as we whish
+    $where = '';
+    
+    // get searcheable_acf, a list of advanced custom fields you want to search content in
+    $list_searcheable_acf = list_searcheable_acf();
+
+    foreach( $exploded as $tag ) :
+        $where .= " 
+          AND (
+            (wp_posts.post_title LIKE '%$tag%')
+            OR (wp_posts.post_content LIKE '%$tag%')
+            OR EXISTS (
+              SELECT * FROM wp_postmeta
+	              WHERE post_id = wp_posts.ID
+	                AND (";
+
+        foreach ($list_searcheable_acf as $searcheable_acf) :
+          if ($searcheable_acf == $list_searcheable_acf[0]):
+            $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+          else :
+            $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+          endif;
+        endforeach;
+
+	        $where .= ")
+            )
+            OR EXISTS (
+              SELECT * FROM wp_comments
+              WHERE comment_post_ID = wp_posts.ID
+                AND comment_content LIKE '%$tag%'
+            )
+            OR EXISTS (
+              SELECT * FROM wp_terms
+              INNER JOIN wp_term_taxonomy
+                ON wp_term_taxonomy.term_id = wp_terms.term_id
+              INNER JOIN wp_term_relationships
+                ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+              WHERE (
+          		taxonomy = 'post_tag'
+            		OR taxonomy = 'category'          		
+            		OR taxonomy = 'myCustomTax'
+          		)
+              	AND object_id = wp_posts.ID
+              	AND wp_terms.name LIKE '%$tag%'
+            )
+        )";
+    endforeach;
+    return $where;
+}
+ 
+add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
+
+
+
+
+
 
 
 		
--- wp-content/themes/starter-theme/page.php
+++ wp-content/themes/starter-theme/page.php
@@ -6,7 +6,8 @@
 
 <?php //use get_template_part( 'loop', 'page' ) if needed ?>
 
-	
+
+
 
 <?php get_sidebar(); ?>
 
