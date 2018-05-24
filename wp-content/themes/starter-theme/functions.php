4e9c354d1ecc9fdd750e654e81f242483102a22b
Garrett Cullen <Garth619@users.noreply.github.com>
2018-05-23 22:16:53 -0700

    push

--- wp-content/themes/starter-theme/functions.php
+++ wp-content/themes/starter-theme/functions.php
@@ -23,6 +23,213 @@ add_theme_support('post-thumbnails');
 
 
 
+/* Rest Api Routes and Endpoints */
+
+
+// states
+
+function list_states() {
+	
+		$terms = get_terms(array(
+			'taxonomy' => 'lawfirm_locations',
+			'orderby'  => 'name',
+			'parent' => 111,
+			'hide_empty' => true
+		));
+		
+		return $terms;
+
+}
+
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'locations/v1', '/states/', array(
+		'methods' => 'GET',
+		'callback' => 'list_states',
+	) );
+} );
+
+
+
+
+//////////// practice
+
+function my_awesome_func( $data ) {
+  $posts = get_posts( array(
+    'author' => $data['id'],
+  ) );
+ 
+  if ( empty( $posts ) ) {
+    return new WP_Error( 'no_author', 'Invalid author', array( 'status' => 404 ) );
+  }
+ 
+  return $posts[0]->post_title;
+  
+}
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'lawyer-route/v1', '/author/(?P<id>\d+)', array(
+    'methods' => 'GET',
+    'callback' => 'my_awesome_func',
+    'args' => array(
+      'id' => array(
+        'validate_callback' => function($param, $request, $key) {
+          return is_numeric( $param );
+        }
+      ),
+    ),
+  ) );
+} );
+
+
+////////////
+
+
+
+
+
+
+
+
+
+function walden_get_all_post_ids() {
+	
+	$terms = get_terms( array(
+        'numberposts' => -1,
+        'taxonomy'   => 'lawfirm_locations',
+        'orderby'  => 'name',
+        'parent' => 0,
+        'child_of' => false
+    ) );
+ 
+    return $terms;
+    
+    
+    // this isn't exact but follow syntax https://wordpress.stackexchange.com/questions/75017/get-tags-specific-category
+    
+    
+    
+    
+}
+ 
+// Add Walden/v1/get-all-post-ids route
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'walden/v1', '/get-all-post-ids/', array(
+		'methods' => 'GET',
+		'callback' => 'walden_get_all_post_ids',
+	) );
+} );
+
+
+
+
+////////////
+
+
+
+
+function prefix_get_endpoint_phrase() {
+    // rest_ensure_response() wraps the data we want to return into a WP_REST_Response, and ensures it will be properly returned.
+    return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
+}
+
+
+function prefix_register_example_routes() {
+    // register_rest_route() handles more arguments but we are going to stick to the basics for now.
+    register_rest_route( 'hello-world/v1', '/phrase', array(
+        // By using this constant we ensure that when the WP_REST_Server changes our readable endpoints will work as intended.
+        'methods'  => WP_REST_Server::READABLE,
+        // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
+        'callback' => 'prefix_get_endpoint_phrase',
+    ) );
+}
+ 
+add_action( 'rest_api_init', 'prefix_register_example_routes' );
+
+
+
+
+
+/////////////
+
+
+
+
+
+/**
+ * This is our callback function to return our products.
+ *
+ * @param WP_REST_Request $request This function accepts a rest request to process data.
+ */
+function prefix_get_products( $request ) {
+    // In practice this function would fetch the desired data. Here we are just making stuff up.
+    $products = array(
+        '1' => 'I am product 1',
+        '2' => 'I am product 2',
+        '3' => 'I am product 3',
+    );
+ 
+    return rest_ensure_response( $products );
+}
+ 
+/**
+ * This is our callback function to return a single product.
+ *
+ * @param WP_REST_Request $request This function accepts a rest request to process data.
+ */
+function prefix_get_product( $request ) {
+    // In practice this function would fetch the desired data. Here we are just making stuff up.
+    $products = array(
+        '1' => 'I am product 1',
+        '2' => 'I am product 2',
+        '3' => 'I am product 3',
+    );
+ 
+    // Here we are grabbing the 'id' path variable from the $request object. WP_REST_Request implements ArrayAccess, which allows us to grab properties as though it is an array.
+    $id = (string) $request['id'];
+ 
+    if ( isset( $products[ $id ] ) ) {
+        // Grab the product.
+        $product = $products[ $id ];
+ 
+        // Return the product as a response.
+        return rest_ensure_response( $product );
+    } else {
+        // Return a WP_Error because the request product was not found. In this case we return a 404 because the main resource was not found.
+        return new WP_Error( 'rest_product_invalid', esc_html__( 'The product does not exist.', 'my-text-domain' ), array( 'status' => 404 ) );
+    }
+ 
+    // If the code somehow executes to here something bad happened return a 500.
+    return new WP_Error( 'rest_api_sad', esc_html__( 'Something went horribly wrong.', 'my-text-domain' ), array( 'status' => 500 ) );
+}
+ 
+/**
+ * This function is where we register our routes for our example endpoint.
+ */
+function prefix_register_product_routes() {
+    // Here we are registering our route for a collection of products.
+    register_rest_route( 'my-shop/v1', '/products', array(
+        // By using this constant we ensure that when the WP_REST_Server changes our readable endpoints will work as intended.
+        'methods'  => WP_REST_Server::READABLE,
+        // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
+        'callback' => 'prefix_get_products',
+    ) );
+    // Here we are registering our route for single products. The (?P<id>[\d]+) is our path variable for the ID, which, in this example, can only be some form of positive number.
+    register_rest_route( 'my-shop/v1', '/products/(?P<id>[\d]+)', array(
+        // By using this constant we ensure that when the WP_REST_Server changes our readable endpoints will work as intended.
+        'methods'  => WP_REST_Server::READABLE,
+        // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
+        'callback' => 'prefix_get_product',
+    ) );
+}
+ 
+add_action( 'rest_api_init', 'prefix_register_product_routes' );
+
+
+
+
+
+
 
 /* Search Tweaks
 -------------------------------------------------------------- */
--- wp-content/themes/starter-theme/page.php
+++ wp-content/themes/starter-theme/page.php
@@ -11,4 +11,142 @@
 
 <?php get_sidebar(); ?>
 
+
+
+<?php // http://www.joshstauffer.com/wordpress-get-tags-for-a-specific-category/
+$custom_query = new WP_Query( 'posts_per_page=-1&category_name=cat-1' );
+if ( $custom_query->have_posts() ) :
+	while ( $custom_query->have_posts() ) : $custom_query->the_post();
+		$posttags = get_the_tags();
+		if ( $posttags ) {
+			foreach( $posttags as $tag ) {
+				$all_tags[] = $tag->term_id;
+			}
+		}
+	endwhile;
+endif;
+
+$tags_arr = array_unique( $all_tags );
+$tags_str = implode( ",", $tags_arr );
+
+$args = array(
+	'smallest'  => 12,
+	'largest'   => 12,
+	'unit'      => 'px',
+	'number'    => 0,
+	'format'    => 'list',
+	'include'   => $tags_str
+);
+wp_tag_cloud( $args );
+?>
+
+
+
+
+
+<?php $args = array(
+    'post_type' => 'lawfirm',
+    'tax_query' => array(
+        'relation' => 'AND',
+        array(
+            'taxonomy' => 'lawfirm_locations',
+            'field'    => 'slug',
+            'terms'    => array( 'california' ),
+        ),
+        array(
+            'taxonomy' => 'lawfirm_practiceareas',
+            'field'    => 'slug',
+            'terms'    => array( 'business' ),
+        ),
+    ),
+);
+$query = new WP_Query( $args );
+
+
+
+
+if ( $query->have_posts() ) {
+ 
+    // Start looping over the query results.
+    while ( $query->have_posts() ) {
+ 
+        $query->the_post();
+ 
+        the_title();
+ 
+    }
+ 
+}
+ 
+// Restore original post data.
+wp_reset_postdata();
+
+
+
+
+?>
+
+<?php 
+
+ 
+$args = array(
+  'taxonomy'     => 'lawfirm_locations',
+  'orderby'      => 'name',
+  'show_count'   => false,
+  'pad_counts'   => false,
+  'hierarchical' => true,
+  'title_li'     => '',
+);
+?>
+ 
+<ul>
+    <?php wp_list_categories( $args ); ?>
+</ul>
+
+
+
+<?php 
+
+ 
+$argstwo = array(
+  'taxonomy'     => 'lawfirm_practiceareas',
+  'orderby'      => 'name',
+  'show_count'   => false,
+  'pad_counts'   => false,
+  'hierarchical' => false,
+  'title_li'     => '',
+);
+?>
+ 
+<ul>
+    <?php wp_list_categories( $argstwo ); ?>
+</ul>
+
+
+
+<ul class="products"> 
+    <?php wp_list_categories('post_type=lawfirm&taxonomy=lawfirm_locations&orderby=id&show_count=1&use_desc_for_title=0&child_of=113'); ?>
+</ul>
+
+
+<?php 
+$taxonomy = 'lawfirm_locations'; //Choose the taxonomy
+$terms = get_terms( $taxonomy ); //Get all the terms
+ 
+foreach ($terms as $term) { //Cycle through terms, one at a time
+ 
+// Check and see if the term is a top-level parent. If so, display it.
+$parent = $term->parent;
+ if ( $parent=='0' ) {
+ 
+	$term_id = $term->term_id; //Define the term ID
+	$term_link = get_term_link( $term, $taxonomy ); //Get the link to the archive page for that term
+	$term_name = $term->name;
+	echo '<a class="ccats" href="' . $term_link . '"><span class="label">' . $term_name . '</span></a>';	
+} }
+?>
+
+
+
+
 <?php get_footer(); ?>
