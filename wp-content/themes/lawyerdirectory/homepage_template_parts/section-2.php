55b5e99c10c0b5bf911f6196f04396ba5f02ba47
Garrett Cullen <Garth619@users.noreply.github.com>
2018-05-27 19:47:44 -0700

    push

--- wp-content/themes/lawyerdirectory/homepage_template_parts/section-2.php
+++ wp-content/themes/lawyerdirectory/homepage_template_parts/section-2.php
@@ -4,6 +4,32 @@
 		
 	<span>Practice Areas</span>
 	
+	
+	<?php 
+		
+		
+	
+	$terms = get_terms( array(
+    'taxonomy' => 'lawfirm_practiceareas',
+	
+	) );
+		
+		if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+     echo '<ul>';
+     foreach ( $terms as $term ) {
+       echo '<li>' . $term->name . '</li>';
+        
+     }
+     echo '</ul>';
+ }
+		
+	?>
+
+	
+	
+	
+	
+	
 	<ul>
 		<li><a href="">Business</a></li>
 		<li><a href="">Business</a></li>
