282126cae18af259d542138849062500a834e982
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-13 12:58:27 -0700

    push

--- wp-content/themes/attorneydirectory/taxonomy-location.php
+++ wp-content/themes/attorneydirectory/taxonomy-location.php
@@ -19,10 +19,31 @@
 
 <h1><?php single_term_title();?></h1>
 
-
-<?php
 	
-	$children = get_queried_object()->term_id;
+<?php	$children = get_queried_object()->term_id;
+	
+	
+	if(get_field('content_blocks','option')) {
+		 		 
+		 echo "<br/><br/>";
+		 			 
+		 	while(has_sub_field('content_blocks','option')) {
+			 			 
+			 if(get_sub_field('current_taxomony') == $children)
+			 			 
+			 		the_sub_field('block');
+		 			 		
+		 		}
+		 		 
+		}	
+
+
+	echo "Browse By City";
+
+	
+	
+	
+	
 	
 	$taxlocations = 'location';
 	$taxpracticeareas = 'office_practice_area';
@@ -51,80 +72,7 @@
      
      }
     
-     // currently using a rewrite rule for this and a page template page-practicearea_city so I can pull the query_vars out of the url and link my terms to the other urls
-/*
-     if( empty($terms)) {
-	     
-	     	echo "Browse By Practice Area<br/></br/>";
-	     
-	   		$termids = get_terms( array( 
-		 			'taxonomy' => $taxpracticeareas,
-		 			'fields' => 'ids',
-		 			)
-		 		);
-
-		 		//print_r($termids);
-		 		
-		 		
-		 		$args = array (
-		 			'post_type' => 'lawfirm',
-		 			'fields' => 'ids',
-		 			'tax_query' => array(
-
-		 			array(
-		 				'taxonomy'  => $taxlocations,
-		 				'field'     => 'ids',
-		 				'terms'     => $children,
-		 			),
-
-		 			array(
-		 				'taxonomy'  => $taxpracticeareas,
-		 				'field'     => 'ids',
-		 				'terms'     => $termids,
-		 			)
-		 		),
-		 	); 			
-
-
-			$postids = new WP_Query( $args );
-
-			
-			while($postids->have_posts()) : $postids->the_post(); 
-			
-			
-			the_title();
-			
-			echo "<br/><br/><br/>";
-                	
-      
-      endwhile; 
-      wp_reset_postdata();
-
-			
-			$termargs = array (
-				'taxonomy' => $taxpracticeareas,
-				//'fields' => 'all_with_object_id',
-				'object_ids' => $postids->posts,
-				//'parent' => $currentparentid,
-			
-			);
-
-			$term_query = new WP_Term_Query( $termargs );
-
-		
-			if ( ! empty( $term_query ) && ! is_wp_error( $term_query ) ) {
-				foreach ( $term_query ->terms as $term )
-				
-					//echo $term;
-				
-			
-					echo '<a href="'. get_bloginfo('url') . "/lawfirm_practiceareas/" .  $term->slug  . '/' . $currentstate . '">' . $term->name . '</a><br/>';
-			
-				}
-
-			}
-*/
-
+    
 	?>
 	
 	</div><!-- section_inner -->
